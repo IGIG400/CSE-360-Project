6d5bc30f7fb4cb0a83926f649758a381625789e2
Isaiah Graham <Isaiah Graham@DESKTOP-6UC6DTH>
2020-10-07 19:40:30 -0700

    Edits have been made

--- CSE360_Assignment1/AddingMachine.java
+++ CSE360_Assignment1/AddingMachine.java
@@ -1,26 +1,49 @@
+/*
+ * Name: Isaiah Graham
+ * Assignment: Assignment 2
+ * Date: 10/7/20
+ */
 package cse360assignment02;
-
+/*
+ * This describes the class of the adding machine 
+ */
 public class AddingMachine {
   private int total;
-  
+ /*
+  * This is the AddigMachine constructor that sets its total to 0
+  */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
-  
+  /*
+   * This method gets the total 
+   */
   public int getTotal () {
     return 0;
   }
-  
+  /*
+   * This is a public method that adds the integer passed in as value to the total
+   */
   public void add (int value) {
   }
-
+/*
+ * This is a public method that subtracts the integer represented by the value from the total
+ */
   public void subtract (int value) {
   }
-
+/*
+ * (non-Javadoc)
+ * @see java.lang.Object#toString()
+ * 
+ * The toString is used to return a string representation of an object
+ */
+ 
   public String toString () {
     return "";
   }
-
+/*
+ * This method will reset the history to 0
+ */
   public void clear() {
   }
 }
